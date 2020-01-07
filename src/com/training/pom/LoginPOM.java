594939b9bfe5715ddcbc6a5b8cecff73d00788e5
SH14732 <shamnitjsr@gmail.com>
2020-01-07 23:09:12 +0530

    1st_commit

---
+++ src/com/training/pom/LoginPOM.java
@@ -0,0 +1,38 @@
+package com.training.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPOM {
+	private WebDriver driver; 
+	
+	public LoginPOM(WebDriver driver) {
+		this.driver = driver; 
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath="//*[@id=\"Email\"]")
+	private WebElement userName; 
+	
+	@FindBy(xpath="//*[@id=\"Password\"]")
+	private WebElement password;
+	
+	@FindBy(xpath="/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/input")
+	private WebElement loginBtn; 
+	
+	public void sendUserName(String userName) {
+		this.userName.clear();
+		this.userName.sendKeys(userName);
+	}
+	
+	public void sendPassword(String password) {
+		this.password.clear(); 
+		this.password.sendKeys(password); 
+	}
+	
+	public void clickLoginBtn() {
+		this.loginBtn.click(); 
+	}
+}
